2002d70d4ed59293b01f7b25a1a2f0ddfb2ce15f
@mohamad66 <rahimi@homacall.com>
2024-03-18 00:55:39 +0330

    dockrr yax0321ok

--- main.go
+++ main.go
@@ -6,11 +6,11 @@ import (
 	//"example.com/ansiblego/Server"
 )
 
-func setupCORS(w *http.ResponseWriter, req *http.Request) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-	(*w).Header().Set("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, PATCH, POST, DELETE")
-	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type, X-Requested-With, Authorization")
-}
+// func setupCORS(w *http.ResponseWriter, req *http.Request) {
+// 	(*w).Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+// 	(*w).Header().Set("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, PATCH, POST, DELETE")
+// 	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type, X-Requested-With, Authorization")
+// }
 
 func main() {
 
@@ -21,6 +21,7 @@ func main() {
 
 	ServerHost := "localhost:9000"
 	errListen := http.ListenAndServe(ServerHost, nil)
+	log.Println("Run ...")
 	if errListen != nil {
 		log.Println(errListen)
 	}
@@ -28,6 +29,6 @@ func main() {
 }
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	setupCORS(&w, r)
-	log.Println(w, "Home Call Ok ...")
+	//setupCORS(&w, r)
+	log.Println("Home Call Ok ...")
 }
