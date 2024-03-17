afc88e2c5577252f5af53b91d28a48391d2724cb
@mohamad66 <rahimi@homacall.com>
2024-03-17 22:28:22 +0330

    dockrr ya

--- Server/server.go
+++ Server/server.go
@@ -1,11 +1,9 @@
 package Server
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 
 	"example.com/ansiblego/Router"
 )
@@ -20,22 +18,23 @@ func Run() {
 	log.Println(" Config Router ...")
 	Router.Route()
 
-	file, err := os.Open("./config.json")
-	if err != nil {
-		log.Println(err)
-	}
-	defer file.Close()
-	config := new(Configserver)
-	errDecode := json.NewDecoder(file).Decode(config)
-	if errDecode != nil {
-		log.Println(errDecode)
-	}
+	// file, err := os.Open("./config.json")
+	// if err != nil {
+	// 	log.Println(err)
+	// }
+	// defer file.Close()
+	// config := new(Configserver)
+	// errDecode := json.NewDecoder(file).Decode(config)
+	// if errDecode != nil {
+	// 	log.Println(errDecode)
+	// }
 
 	fmt.Print(" server run")
 	log.Println("Start http server.")
 
-	ServerHost := config.Host+":"+config.Port
-	errListen := http.ListenAndServe(ServerHost,nil)
+	//ServerHost := config.Host+":"+config.Port
+	ServerHost := "localhost:9000"
+	errListen := http.ListenAndServe(ServerHost, nil)
 	if errListen != nil {
 		fmt.Println(errListen)
 	}
